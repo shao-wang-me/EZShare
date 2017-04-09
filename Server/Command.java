a5e9fab097995d500d303cf2879232959a692c59
wsdonny <wsdonny@gmail.com>
2017-04-09 14:27:29 +1000

    Update Command.java
    Three commands: publish, remove, share.

--- Server/Command.java
+++ Server/Command.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,13 +18,61 @@ public class Command {
 			 */
 			if (resource.isFile() || 
 					(resourceMap.containsKey(resource.getKey()) && 
-							resourceMap.get(resource.getKey()).getOwner() == resource.getOwner())
+							resourceMap.get(resource.getKey()).getOwner() != resource.getOwner())
 					) {
 				toReturn.put(false, "cannot publish resource");
+			} else {
+				/**Now we should be able to add or update the resource.*/
+				resourceMap.put(resource.getKey(), resource);
+				toReturn.put(true, "success");
 			}
-			/**Now we should be able to add or update the resource.*/
-			resourceMap.put(resource.getKey(), resource);
-			toReturn.put(true, "success");
+		}
+		return toReturn;
+	}
+	
+	public Map<Boolean, String> remove(Resource resource, Map<String, Resource> resourceMap) {
+		Map<Boolean, String> toReturn = new HashMap<Boolean, String>();
+		if (!resource.isValid()) {
+			toReturn.put(false, "invalid resource");
+		} else {
+			if (resourceMap.containsKey(resource.getKey()) && resourceMap.get(resource.getKey()).getOwner() == resource.getOwner()) {
+				resourceMap.remove(resource.getKey());//The remove operation.
+				toReturn.put(true, "success");
+			} else {
+				/**Cannot remove resource: the resource did not exist.*/
+				toReturn.put(false, "cannot remove resource");
+			}
+		}
+		return toReturn;
+	}
+	
+	public Map<Boolean, String> share(Resource resource, Map<String, Resource> resourceMap) throws URISyntaxException {
+		Map<Boolean, String> toReturn = new HashMap<Boolean, String>();
+		if (!resource.isValid()) {
+			toReturn.put(false, "invalid resource");
+		} else {
+			/**
+			 * Cannot publish resource:
+			 * 1. The resource is not a file.
+			 * 2. Same channel, same URI, different owner.
+			 * 3. Not pointing to a file on the local file system.
+			 * 4. Cannot be read as a file.
+			 */
+			if (!resource.isFile() || 
+					(resourceMap.containsKey(resource.getKey()) && 
+							resourceMap.get(resource.getKey()).getOwner() != resource.getOwner())
+					) {
+				toReturn.put(false, "cannot share resource");
+			} else {
+				File file = new File(resource.getURI());
+				if (file.canRead()) {
+					/**Now we should be able to add or update the resource.*/
+					resourceMap.put(resource.getKey(), resource);
+					toReturn.put(true, "success");
+				} else {
+					toReturn.put(false, "cannot share resource");
+				}				
+			}
 		}
 		return toReturn;
 	}
