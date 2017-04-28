6d0c167abb871a9b67d093b37066a5214850d582
MarcosQiu <anjiqyk@gmail.com>
2017-04-28 17:56:49 +1000

    Add files via upload

--- DSProjectClient/src/Client.java
+++ DSProjectClient/src/Client.java
@@ -1,13 +1,25 @@
 import java.io.*;
 import java.net.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.json.*;
 
 public class Client {
 
 	private Socket s = null;
+	private Logger log = null;
 
 	Client(String serverIP, int serverPort) {
+		this.setLog();
+		LogFormatter formatter = new LogFormatter();
+       ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(formatter);
+		Logger logIni = Logger.getLogger(Main.class.getName());
+		logIni.setUseParentHandlers(false);
+		logIni.setLevel(Level.INFO);
+		logIni.info("setting debug on");
 		try {
 			s = new Socket(serverIP, serverPort);
 		} catch (UnknownHostException e) {
@@ -17,25 +29,48 @@ public class Client {
 		}
 	}
 
-	public void sendJSON(JSONObject j,boolean ifDebug) throws JSONException {
+	public void setLog() {
+    	LogFormatter formatter = new LogFormatter();
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(formatter);
+        
+		Logger log = Logger.getLogger(Main.class.getName());
+		log.setUseParentHandlers(false);
+		log.addHandler(handler);
+		log.setLevel(Level.FINE);
+		this.log = log;
+	}
+	
+	public void sendJSON(JSONObject j,boolean ifDebug,String host,String port) throws JSONException {
 		try {
+			
 			DataInputStream input = new DataInputStream(s.getInputStream());
 			DataOutputStream output = new DataOutputStream(s.getOutputStream());
 
 			if (ifDebug) {
-				System.out.println("SENT:" + j.toString());
+				//log.info(j.getString("command").toLowerCase() + "ing to " + host + ":" + port);
+				log.info("SENT:" + j.toString());
+
 			}
 			
 			output.writeUTF(j.toString());
 			output.flush();
+			
+			
 
 			String message = "";
-			while (true) {
+			long startTime = System.currentTimeMillis();
+			long currentTime = 0;
+			while ((currentTime = System.currentTimeMillis()) - startTime <= 5*60*1000) {
 				JSONObject msgGet = null;
 				if (input.available() > 0) {
 					message = input.readUTF();
 					msgGet = new JSONObject(message);
-					System.out.println(ifDebug?"RECEIVED:" + msgGet.toString():msgGet.toString());
+					if(ifDebug) {
+						log.info("RECEIVED:" + msgGet.toString());
+					} else {
+						System.out.println(msgGet.toString());
+					}
 					if (j.getString("command").equalsIgnoreCase("QUERY") && msgGet.has("resultSize")) {
 						break;
 					} else if (!j.getString("command").equalsIgnoreCase("QUERY") && msgGet.has("response")) {
@@ -43,19 +78,35 @@ public class Client {
 					}
 				}
 			}
+			if (currentTime - startTime > 5*60*1000) {
+				System.out.println("Time out");
+				System.exit(0);
+			}
 		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			System.out.println(e.getMessage());
+			System.exit(-1);
+		} catch (SocketException e) {
+			System.out.println(e.getMessage());
+			System.exit(-1);
 		} catch (IOException e) {
 			//Do something
-		}
+			System.out.println(e.getMessage());
+			System.exit(-1);
+		} 
 	}
 
-	public void fetch(JSONObject j) throws JSONException {
+	public void fetch(JSONObject j,boolean ifDebug,String host,String port) throws JSONException {
 		int resourceSize = 0;
 		String uri = "";
 		try {
+			
 			DataInputStream input = new DataInputStream(s.getInputStream());
 			DataOutputStream output = new DataOutputStream(s.getOutputStream());
+			
+			if (ifDebug) {
+				log.info("fetching from " + host + ":" + port);
+				log.info("SENT:" + j.toString());
+			}
 
 			output.writeUTF(j.toString());
 			output.flush();
@@ -65,14 +116,22 @@ public class Client {
 			while (true) {
 				if (input.available() > 0) {
 					message = input.readUTF();
-					System.out.println(message);
 					msgGet = new JSONObject(message);
+					if(ifDebug) {
+						log.info("RECEIVED:" + msgGet.toString());
+					} else {
+						System.out.println(msgGet.toString());
+					}
 				}
 
 				// fetch the file using the uri and resource size
 
 				if (msgGet != null && msgGet.has("resourceSize")) {
 					resourceSize = msgGet.getInt("resourceSize");
+					if (Runtime.getRuntime().maxMemory() < resourceSize) {
+						System.out.println("Out of memory");
+						System.exit(-1);
+					}
 					uri = msgGet.getString("uri");
 					
 					String filename = uri.split("/")[uri.split("/").length - 1];
@@ -107,9 +166,14 @@ public class Client {
 				}
 			}
 		} catch (UnknownHostException e) {
-			e.printStackTrace();
+			System.out.println(e.getMessage());
+			System.exit(-1);
+		} catch (SocketException e) {
+			System.out.println(e.getMessage());
+			System.exit(-1);
 		} catch (IOException e) {
-
+			System.out.println(e.getMessage());
+			System.exit(-1);
 		}
 	}
 
--- DSProjectClient/src/Main.java
+++ DSProjectClient/src/Main.java
@@ -1,15 +1,53 @@
 
-import java.io.*;
-import java.net.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+
 import org.apache.commons.cli.*;
 import org.json.*;
 
 public class Main {
 
 	public static void main(String[] args) throws JSONException {
-		int serverPort = 3000;
+		Integer serverPort = 10000;
 		String serverIP = "localhost";
+
+		// String serverIP = "sunrise.cis.unimelb.edu.au";
+		// int serverPort = 3781;
+
+		/*
+		 * Test module
+		 */
+
+		/*
+		 * 
+		 * Socket s = null; try { s = new Socket(serverIP, serverPort); } catch
+		 * (UnknownHostException e) { e.printStackTrace(); } catch (IOException
+		 * e) { e.printStackTrace(); } JSONObject j = new JSONObject("{}");
+		 * JSONObject re = new JSONObject("{}"); ArrayList<String> tag = new
+		 * ArrayList<String>(); ArrayList<JSONObject> servers = new
+		 * ArrayList<JSONObject>(); JSONObject serverOne = new JSONObject("{}");
+		 * JSONObject serverTwo = new JSONObject("{}");
+		 * serverOne.put("host","10.13.62.119"); serverOne.put("port",10000);
+		 * serverTwo.put("host", "gg"); serverTwo.put("port","1234");
+		 * servers.add(serverTwo); //tag.add("QUERY"); //tag.add("PUBLISH");
+		 * j.put("command", "QUERY"); j.put("relay", true); re.put("name", "");
+		 * re.put("tags", tag); re.put("description",""); re.put("uri", "");
+		 * re.put("channel", 11); //re.put("owner","");
+		 * j.put("resourceTemplate",re); System.out.println(j.toString()); try {
+		 * DataInputStream input = new DataInputStream(s.getInputStream());
+		 * DataOutputStream output = new DataOutputStream(s.getOutputStream());
+		 * output.writeUTF(j.toString()); output.flush(); String message = "";
+		 * long startTime = System.currentTimeMillis(); long currentTime = 0;
+		 * while ((currentTime = System.currentTimeMillis()) - startTime <=
+		 * 50000000) { JSONObject msgGet = null; if (input.available() > 0) {
+		 * message = input.readUTF(); msgGet = new JSONObject(message);
+		 * System.out.println(msgGet.toString()); } } } catch (IOException e) {
+		 * // TODO Auto-generated catch block e.printStackTrace(); }
+		 */
 
 		Options option = new Options();
 		option.addOption("channel", true, "channel");
@@ -37,79 +75,84 @@ public class Main {
 			// get the host name and port number
 			cmd = parser.parse(option, args);
 		} catch (ParseException e) {
-			// something needs to be done
+			System.out.println(e.getMessage());
+			System.exit(-1);
 		}
 
 		if (cmd.hasOption("host")) {
 			serverIP = cmd.getOptionValue("host");
 		}
-
 		if (cmd.hasOption("port")) {
 			serverPort = Integer.parseInt(cmd.getOptionValue("port"));
 		}
+
 		Client client = new Client(serverIP, serverPort);
-		
-		
+
 		if (cmd.hasOption("publish")) {
 			// publish command
-			try{
+			try {
 				Operations.Publish(cmd, client);
 			} catch (JSONException e) {
-				//Do something
+				// Do something
+				System.out.println(e.getMessage());
+				System.exit(-1);
 			}
 		}
-		
+
 		if (cmd.hasOption("query")) {
 			// publish command
-			try{
+			try {
 				Operations.Query(cmd, client);
-			} catch (JSONException e){
-				//Do something
+			} catch (JSONException e) {
+				// Do something
+				System.out.println(e.getMessage());
+				System.exit(-1);
 			}
 		}
 
 		if (cmd.hasOption("remove")) {
-			try{
+			try {
 				Operations.Remove(cmd, client);
 			} catch (JSONException e) {
-				//Do something
+				// Do something
+				System.out.println(e.getMessage());
+				System.exit(-1);
 			}
 		}
-		
+
 		if (cmd.hasOption("share")) {
-			try{
+			try {
 				Operations.Share(cmd, client);
 			} catch (JSONException e) {
-				//Do something
+				// Do something
+				System.out.println(e.getMessage());
+				System.exit(-1);
 			}
 		}
-		
+
 		if (cmd.hasOption("fetch")) {
-			try{
+			try {
 				Operations.Fetch(cmd, client);
 			} catch (JSONException e) {
-				//Do something
+				// Do something
+				System.out.println(e.getMessage());
+				System.exit(-1);
 			}
 		}
-		
+
 		if (cmd.hasOption("exchange")) {
-			try{
+			try {
 				Operations.Exchange(cmd, client);
 			} catch (JSONException e) {
-				//Do something
+				// Do something
+				System.out.println(e.getMessage());
+				System.exit(-1);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Missing \"-servers\"");
+				System.exit(-1);
 			}
 		}
-		/*
-		 * JSONObject j = new JSONObject("{}"); JSONObject resource = new
-		 * JSONObject("{}"); ArrayList<String> l = new ArrayList<String>();
-		 * ServerInfo s = null; resource.put("name",""); resource.put("tags",l);
-		 * resource.put("description", ""); resource.put("uri", "");
-		 * resource.put("channel",""); resource.put("owner", "");
-		 * resource.put("ezserver",s); j.put("command","QUERY"); j.put("relay",
-		 * true); j.put("resourceTemplate", resource);
-		 * System.out.println(resource.toString());
-		 * System.out.println(j.toString()); client.query(j);
-		 */
+
 	}
 
 }
