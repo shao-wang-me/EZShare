5eaabf110cc98ac34443c1d048a08e0d2a33d097
MarcosQiu <anjiqyk@gmail.com>
2017-04-29 12:26:44 +1000

    Update Client.java

--- Server/client/Client.java
+++ Server/client/Client.java
@@ -19,6 +19,8 @@ public class Client {
 		String serverIP = "localhost";
 
 		Options option = new Options();
+		option.addOption("h",false,"help documentation");
+		option.addOption("-help",false,"help documentation");
 		option.addOption("channel", true, "channel");
 		option.addOption("debug", false, "print debug information");
 		option.addOption("description", true, "resource description");
@@ -47,6 +49,26 @@ public class Client {
 			System.exit(-1);
 		}
 
+		if (cmd.hasOption("h") || cmd.hasOption("-help")) {
+			System.out.println("-channel <args>       channel");
+			System.out.println("-debug                print debug information");
+			System.out.println("-description <args>   resource description");
+			System.out.println("-exchange             exchange server list with server");
+			System.out.println("-fetch                fetch resources from server");
+			System.out.println("-host <args>          server host, a domain name or IP address");
+			System.out.println("-name <args>          resource name");
+			System.out.println("-owner <args>         owner");
+			System.out.println("-port <args>          server port, an integer");
+			System.out.println("-publish              publish resource on server");
+			System.out.println("-query                query for resources from server");
+			System.out.println("-remove               remove resource from server");
+			System.out.println("-secret <args>        secret");
+			System.out.println("-servers <args>       server list, host1:port1,host2:port2,...");
+			System.out.println("-share                share resource on server");
+			System.out.println("-tags <args>          resource tags, tag1,tag2,tag3,...");
+			System.out.println("-uri <args>           resource URI");
+		}
+		
 		if (cmd.hasOption("host")) {
 			serverIP = cmd.getOptionValue("host");
 		}
