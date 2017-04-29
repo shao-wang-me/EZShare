134b3f18593d87b5521ee7e3c420894c962f284d
wsdonny <wsdonny@gmail.com>
2017-04-29 20:20:02 +1000

    Update resourceList.java

--- Server/variable/resourceList.java
+++ Server/variable/resourceList.java
@@ -13,7 +13,7 @@ public class resourceList {
 			resourceList.add(r);
 		} else {
 			Resource sameResource = getSameResource(r);
-			if (!r.getOwner().equals(sameResource.getOwner())) {
+			if (r.getOwner().equals(sameResource.getOwner())) {
 				resourceList.remove(sameResource);
 				resourceList.add(r);
 			}
